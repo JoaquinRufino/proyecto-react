60b452283edd07b76b1b33ba36fb3ebc2dd515ac
Joaquin Rufino <rufinojoaquin10@gmail.com>
2023-08-05 19:36:24 -0300

    "color"

--- src/components/pages/formFormik/FormFormik.jsx
+++ src/components/pages/formFormik/FormFormik.jsx
@@ -28,16 +28,6 @@ const FormFormik = () => {
       .required("Este campo es obligatorio")
       .matches(/^[0-9]+$/, "Debe ser un numero"),
 
-
-      //password: Yup.string()
-      //.required("Este campo es obligatorio")
-      //.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/, {
-      //  message:
-      //"La contraseña debe tener al menos 1 mayuscula y 6 caracteres ",
-      //}),
-      //repet: Yup.string()
-      //.required("Este campo es obligatorio")
-      //.oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
     }),
     validateOnChange: false,
   });
@@ -64,43 +54,6 @@ const FormFormik = () => {
             error={errors.email ? true : false}
             helperText={errors.email}
           />
-          {/*<FormControl variant="filled">
-          <OutlinedInput
-            style={{ width: "400px" }}
-            label="Password"
-            variant="outlined"
-            name="password"
-            onChange={handleChange}
-            error={errors.password ? true : false}
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Password
-                </InputLabel>
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-          </FormControl>
-
-          <TextField
-          type="text"
-          label="Repet"
-          variant="outlined"
-          name="repet"
-          onChange={handleChange}
-          error={errors.repet ? true : false}
-          helperText={errors.repet}
-          />*/}
           <TextField
             type="text"
             label="Phone"
