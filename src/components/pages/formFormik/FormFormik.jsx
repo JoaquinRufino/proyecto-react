316e544bc07a083aa7ce093622c34476085d7487
Joaquin Rufino <rufinojoaquin10@gmail.com>
2023-08-05 20:13:47 -0300

    formik

--- src/components/pages/formFormik/FormFormik.jsx
+++ src/components/pages/formFormik/FormFormik.jsx
@@ -42,7 +42,7 @@ const FormFormik = () => {
             type="text"
             label="Nombre"
             InputLabelProps={{
-              style: { color: 'white', borderColor: 'white' },
+              style: { color: 'white' },
             }}
             variant="outlined"
             name="name"
@@ -54,7 +54,7 @@ const FormFormik = () => {
             type="text"
             label="Email"
             InputLabelProps={{
-              style: { color: 'white', borderColor: 'white' },
+              style: { color: 'white' },
             }}
             variant="outlined"
             name="email"
@@ -66,7 +66,7 @@ const FormFormik = () => {
             type="text"
             label="Phone"
             InputLabelProps={{
-              style: { color: 'white', borderColor: 'white' },
+              style: { color: 'white' },
             }}
             variant="outlined"
             name="phone"
